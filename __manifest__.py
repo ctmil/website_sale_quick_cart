80f6a84f03d74942ea0a0967658a961fd28dd64c
Ignacio Buioli <ibuioli@gmail.com>
2020-08-03 11:58:48 -0300

    feat: initial commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Add-to-Cart Quick online shopping",
+    "version": "12.0.0.1",
+    "author": "Moldeo Interactive",
+    "license": "",
+    "category": "eCommerce",
+    "website": "https://www.moldeointeractive.com.ar",
+    "depends": ["website_sale"],
+    "images": ["images/shop.png"],
+    "data": ["views/website_sale_add_to_cart_views.xml"],
+    "installable": True,
+}
